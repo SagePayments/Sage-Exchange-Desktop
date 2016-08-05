da33961751c5f952e3042a3b8d34e1fd9e5d901a
majid-heart-sage <majid.razvi@sage.com>
2016-08-04 21:35:37 -0600

    added comments to sale sample

--- Samples/SageExchangeDesktopSamples/Program.cs
+++ Samples/SageExchangeDesktopSamples/Program.cs
@@ -1,89 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using clSedApi;
-using SpsXmlCL.Client;
 
 namespace SageExchangeDesktopSamples
 {
-    partial class Program
+    class Program
     {
-        const double paymentAmount = 1.00;
-        const string merchantId = "999999999997";
-        const string merchantKey = "K3QD6YWyhfD";
-        const string applicationId = "DEMO";
-        const string languageId = "EN";
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Press any key to attempt transaction.");
+            Console.WriteLine("Press any key to begin samples.");
             Console.ReadLine();
+            // -----------------------------------------------
 
-            var sedClient = new ModuleClient();
-            
-            var payment = new Request_v1.PaymentType();
+            // The Config class shares data between requests. Let's get it set up now:
+            Config.merchantId = "999999999997";
+            Config.merchantKey = "K3QD6YWyhfD";
+            Config.applicationId = "DEMO";
+            Config.languageId = "EN";
+            Config.paymentAmount = 1.00;
 
-            payment.Merchant = new Request_v1.MerchantType
-            {
-                MerchantID = merchantId,
-                MerchantKey = merchantKey
-            };
+            // The Samples class contains various methods to demonstrate various common scenarios.
+            // Each sample method is stored in a file of the same name; eg, Sale() is in Sale.cs.
 
-            payment.TransactionBase = new Request_v1.TransactionBaseType
-            {
-                TransactionType = "11",
-                TransactionID = Guid.NewGuid().ToString(),
-                Reference1 = "Invoice " + (new Random()).Next(0, 100).ToString(),
-                Amount = paymentAmount
-            };
+            // Running a simple Sale transaction is a good way to get started:
+            Samples.Sale();
 
-            payment.Customer = new Request_v1.PersonType
-            {
-                Name = new Request_v1.NameType
-                {
-                    FirstName = "John",
-                    MI = "Q",
-                    LastName = "Developer"
-                }
-                // ... Address = new Request_v1.AddressType...
-            };
-
-            Request_v1 sedRequest = new Request_v1();
-
-            sedRequest.Payments = new Request_v1.PaymentType[1];
-            sedRequest.Payments[0] = payment;
-
-            sedRequest.Application = new Request_v1.ApplicationType
-            {
-                ApplicationID = applicationId,
-                LanguageID = languageId
-            };
-
-            
-            ModuleResponse sedResponse = (ModuleResponse)sedClient.GetResponse(sedRequest.ToXml());
-
-            Console.WriteLine("Status Code: " + sedResponse.GetStatusCode().ToString());
-            Console.WriteLine("Status Desc: " + sedResponse.GetStatusDescription().ToString());
+            // -----------------------------------------------
             Console.WriteLine();
-
-            // uncomment to print out the raw xml response
-            //Console.WriteLine("Response Length: " + sedResponse.GetResponseLength());
-            //Console.WriteLine("Response Text: " + sedResponse.GetResponseText());
-            //Console.WriteLine();
-
-            Response_v1 paymentResponse = Response_v1.FromXml(sedResponse.GetResponseText());
-            Console.WriteLine("Response Message: " + paymentResponse.PaymentResponses[0].Response.ResponseMessage);
-            Console.WriteLine("Transacion Reference: " + paymentResponse.PaymentResponses[0].TransactionResponse.VANReference);
-            Console.WriteLine();
-
             Console.WriteLine("Press any key to quit.");
-            Console.ReadLine();
-
-            
+            Console.ReadLine(); 
         }
-
     }
 }
